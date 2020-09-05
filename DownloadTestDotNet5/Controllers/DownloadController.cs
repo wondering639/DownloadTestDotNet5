c5596c3b5a22dec8c29898c56d6a7eb8c9344c1c
wondering639 <25722498+wondering639@users.noreply.github.com>
2020-09-05 17:32:46 +0200

    Add project files.

---
+++ DownloadTestDotNet5/Controllers/DownloadController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DownloadTestDotNet5.Controllers
+{
+    public class DownloadController : ControllerBase
+    {
+
+        [HttpGet("~/download/{downloadId}")]
+        public async Task<IActionResult> Download(long downloadId)
+        {
+            var buffer = Encoding.UTF8.GetBytes("Hello! Content is here.");
+            var stream = new MemoryStream(buffer);
+
+            var result = new FileStreamResult(stream, "text/plain");
+            result.FileDownloadName = "test.txt";
+            return result;
+        }
+    }
+}
